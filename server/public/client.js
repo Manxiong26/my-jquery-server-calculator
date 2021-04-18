aa8d92c22ccd232bc6c14919bed18baae41c7e82
Maneena Xiong <xiong.maneena@gmail.com>
2021-04-17 23:47:00 -0500

    created my Get and POST

--- server/public/client.js
+++ server/public/client.js
@@ -2,6 +2,54 @@ console.log('hi js');
 
 $(document).ready(onReady)
 
+let operation = '';
+
 function onReady(){
     console.log('hi jQ');
 }
+
+function calcInput(){
+    let input = {};
+    input.fristNum = $(`#input-one`).val();
+    input.secondNum = $(`#input-two`).val();
+    console.log('operation', operation);
+    input.operator = operation;
+    console.log('operation is sent to server', input.operator);
+    console.log('result to formulate calc', input);
+      
+}
+
+function doMath(){
+    operation = $(this).attr('value');
+}
+
+function postCalc(obj) {
+    $.ajax({
+        method: 'POST',
+        url: '/calculator',
+        data: obj
+    })
+    .then(function (response) {
+        console.log('Response from server', response);  
+    })
+    .catch(function( error){
+        console.log('Error', error);
+        alert('Something bad HAPPENED. Try again later');
+    })
+}
+
+function getCalc(){
+    $.ajax({
+        method: 'GET',
+        url: '/calculator'
+    })
+    .then(function( response) {
+        console.log('Got message', response);
+        render(response);
+        
+    })
+    .catch(function (error){
+        console.log('Error from server', error); 
+    })
+}
+
